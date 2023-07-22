c9b2d2623a28e0aedff1860824c3f797790cfd9f
sharontan6217 <52969503+sharontan6217@users.noreply.github.com>
2023-07-22 22:59:32 +0800

    add files

---
+++ command_powershell.bat
@@ -0,0 +1,21 @@
+@echo off
+
+echo please enter the location of "interview_data_set.gz"
+echo eg: c:/users/admin (*note: please DO NOT include the slash in the end)
+set /p inputDir=Please input the directory:
+cd %inputDir%
+mkdir log
+pause
+copy interview_data_set.gz %inputDir%\log\interview_data_set.gz
+cd %inputDir%
+echo %inputDir% > input_dir.txt
+
+pause
+
+
+python log_analysis.py
+
+copy log.json %inputDir%\log\log.json
+
+powershell .\post_powershell.ps1
+pause
